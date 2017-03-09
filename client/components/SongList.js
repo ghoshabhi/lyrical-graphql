57c947e1e58610edee333e72e63eb274c6f1cf84
Abhishek Ghosh <abhighosh18@gmail.com>
2017-03-09 13:55:00 -0800

    Feat: Refetch records after creating new record

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
 import { Link } from 'react-router';
+import query from '../queries/fetchSongs';
 
 class SongList extends Component {
   renderSongs() {
@@ -27,7 +28,7 @@ class SongList extends Component {
         {this.renderSongs()}
         </ul>
         <Link
-          to="songs/new"
+          to="/songs/new"
           className="btn-floating btn-large red right"
         >
           <i className="material-icons">add</i>
@@ -37,13 +38,4 @@ class SongList extends Component {
   }
 };
 
-const query = gql`
-  {
-    songs {
-      title
-    }
-  }
-`;
-
 export default graphql(query)(SongList);
- ;
